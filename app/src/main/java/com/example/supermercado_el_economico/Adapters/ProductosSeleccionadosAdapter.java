493510b17e7f7d5af3ea2f8bb2bac471740bc5c6
kevincastellon <158013293+kevincastellon@users.noreply.github.com>
2024-04-04 21:58:02 -0600

    Carrito

--- app/src/main/java/com/example/supermercado_el_economico/Adapters/ProductosSeleccionadosAdapter.java
+++ app/src/main/java/com/example/supermercado_el_economico/Adapters/ProductosSeleccionadosAdapter.java
@@ -1,10 +1,11 @@
-package com.example.supermercado_el_economico;
+package com.example.supermercado_el_economico.Adapters;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.supermercado_el_economico.R;
+import com.example.supermercado_el_economico.SharedPreferencesHelper;
 import com.example.supermercado_el_economico.models.Producto;
 
 import java.util.List;
@@ -37,8 +40,12 @@ public class ProductosSeleccionadosAdapter extends RecyclerView.Adapter<Producto
     public void onBindViewHolder(@NonNull ProductoViewHolder holder, @SuppressLint("RecyclerView") int position) {
         Producto producto = productosSeleccionados.get(position);
         holder.textViewNombreProducto.setText(producto.getNombre());
-        holder.textViewPrecioProducto.setText("Lps/. " + producto.getPrecio());
-        holder.textViewCantidad.setText("Cantidad: " + producto.getCantidad());
+        //holder.textViewPrecioProducto.setText("" + producto.getPrecio());
+        Double Precio = producto.getPrecio() * producto.getCantidad();
+        holder.textViewPrecioProducto.setText(String.valueOf(Precio));
+        //holder.textViewPrecioProducto.setText(String.valueOf(producto.getPrecio()));
+        //holder.textViewCantidad.setText("Cantidad: " + producto.getCantidad());
+        holder.textViewCantidad.setText(String.valueOf(producto.getCantidad()));
         Glide.with(context)
                 .load(producto.getFoto())
                 .placeholder(R.drawable.placeholder_image)
@@ -47,10 +54,49 @@ public class ProductosSeleccionadosAdapter extends RecyclerView.Adapter<Producto
         holder.btnRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int newQuantity = producto.getCantidad() - producto.getCantidad();
+               // if (newQuantity >= 0) {
+                    producto.setCantidad(newQuantity);
+                //    holder.textViewCantidad.setText("Cantidad: " + newQuantity);
+                    if (newQuantity == 0) {
+                        productosSeleccionados.remove(position);
+                        notifyItemRemoved(position);
+                        notifyItemRangeChanged(position, productosSeleccionados.size());
+                    }
+                    SharedPreferencesHelper.saveProductos(context, productosSeleccionados);
+           //     }
+            }
+        });
+
+        holder.btnmenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 int newQuantity = producto.getCantidad() - 1;
                 if (newQuantity >= 0) {
+                producto.setCantidad(newQuantity);
+                    Double newPrecio = producto.getPrecio() * newQuantity;
+                    holder.textViewPrecioProducto.setText(String.valueOf(newPrecio));
+                    holder.textViewCantidad.setText(String.valueOf(newQuantity));
+                if (newQuantity == 0) {
+                    productosSeleccionados.remove(position);
+                    notifyItemRemoved(position);
+                    notifyItemRangeChanged(position, productosSeleccionados.size());
+                }
+                SharedPreferencesHelper.saveProductos(context, productosSeleccionados);
+                     }
+            }
+        });
+
+        holder.btnmas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newQuantity = producto.getCantidad() + 1;
+                if (newQuantity >= 0) {
                     producto.setCantidad(newQuantity);
-                    holder.textViewCantidad.setText("Cantidad: " + newQuantity);
+                    Double newPrecio = producto.getPrecio() * newQuantity;
+                    holder.textViewPrecioProducto.setText(String.valueOf(newPrecio));
+                    holder.textViewCantidad.setText(String.valueOf(newQuantity));
+
                     if (newQuantity == 0) {
                         productosSeleccionados.remove(position);
                         notifyItemRemoved(position);
@@ -60,6 +106,9 @@ public class ProductosSeleccionadosAdapter extends RecyclerView.Adapter<Producto
                 }
             }
         });
+
+
+
     }
 
     @Override
@@ -73,6 +122,7 @@ public class ProductosSeleccionadosAdapter extends RecyclerView.Adapter<Producto
         TextView textViewPrecioProducto;
         TextView textViewCantidad;
         ImageView btnRemove;
+        Button btnmenos,btnmas;
 
         public ProductoViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -81,6 +131,8 @@ public class ProductosSeleccionadosAdapter extends RecyclerView.Adapter<Producto
             textViewPrecioProducto = itemView.findViewById(R.id.precioproduto);
             textViewCantidad = itemView.findViewById(R.id.textCantidad);
             btnRemove = itemView.findViewById(R.id.btnremove);
+            btnmas = itemView.findViewById(R.id.btnMas);
+            btnmenos = itemView.findViewById(R.id.btnMenos);
         }
     }
 }
--- app/src/main/java/com/example/supermercado_el_economico/CarritoActivity.java
+++ app/src/main/java/com/example/supermercado_el_economico/CarritoActivity.java
@@ -1,11 +1,13 @@
 package com.example.supermercado_el_economico;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.supermercado_el_economico.Adapters.ProductosSeleccionadosAdapter;
 import com.example.supermercado_el_economico.models.Producto;
 
 import java.util.List;
@@ -20,11 +22,18 @@ public class CarritoActivity extends AppCompatActivity {
         // Cargar la lista de productos seleccionados desde SharedPreferences
         List<Producto> productosSeleccionados = SharedPreferencesHelper.loadProductos(this);
 
-        // Configurar la vista para mostrar los productos seleccionados, por ejemplo, utilizando un RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.recyclerViewProductosSeleccionados);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        ProductosSeleccionadosAdapter adapter = new ProductosSeleccionadosAdapter(productosSeleccionados);
-        recyclerView.setAdapter(adapter);
+// Verificar si la lista está vacía
+        if (productosSeleccionados.isEmpty()) {
+            // La lista está vacía, mostrar un mensaje
+            Toast.makeText(this, "El carrito esta vacío", Toast.LENGTH_SHORT).show();
+        } else {
+            // La lista no está vacía, configurar la vista para mostrar los productos seleccionados
+            RecyclerView recyclerView = findViewById(R.id.recyclerViewProductosSeleccionados);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            ProductosSeleccionadosAdapter adapter = new ProductosSeleccionadosAdapter(productosSeleccionados);
+            recyclerView.setAdapter(adapter);
+        }
+
     }
 
 }
