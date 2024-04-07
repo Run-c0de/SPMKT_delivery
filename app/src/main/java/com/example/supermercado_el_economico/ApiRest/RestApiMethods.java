713dbc584601b84ecd544b5fc99a00be176c027f
Keyla Soriano <44504724+ksoriano55@users.noreply.github.com>
2024-04-07 12:14:06 -0600

    Direcciones
    - Se agrega funcionalidad al listado de direcciones

--- app/src/main/java/com/example/supermercado_el_economico/ApiRest/RestApiMethods.java
+++ app/src/main/java/com/example/supermercado_el_economico/ApiRest/RestApiMethods.java
@@ -1,5 +1,8 @@
 package com.example.supermercado_el_economico.ApiRest;
 
 public class RestApiMethods {
+    private static final String URLAPI= "https://delivery-service.azurewebsites.net/api/";
     public static final String EndPointCreatePerson = "https://delivery-service.azurewebsites.net/api/usuarios";
+    public static final String EndPointGetDirecciones = URLAPI + "Direccion?usuarioId=";
+
 }
